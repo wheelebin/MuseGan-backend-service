6ef683cf887143abc198ecff343e29f225792f5b
Your Name <you@example.com>
2021-07-06 23:13:51 +0200

    stuff

--- music_generation_service.py
+++ music_generation_service.py
@@ -8,8 +8,8 @@ import config
 
 class MusicGenKing:
     def __init__(self, generator_file_path):
-        #self.genOne = GenOne(generator_file_path)
-        #self.genTwo = GenTwo()
+        self.genOne = GenOne(generator_file_path)
+        self.genTwo = GenTwo()
         self.genThree = GenThree()
         self.genFour = GenFour()
 
@@ -111,5 +111,5 @@ def get_wav_by_name(file_name):
 
     if len(wav_files) == 0:
         return None
-        
+
     return wav_files[0]
